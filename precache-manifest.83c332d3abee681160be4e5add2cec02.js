bf8ef46ce63fc0c89fec678f0199c3185a787d94
Umar Farooq <onskadthecamel786@outlook.com>
2021-10-14 03:59:08 +0100

    Updates

---
+++ precache-manifest.83c332d3abee681160be4e5add2cec02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e825a68bc1a5a9f91c50c08b09f57e",
+    "url": "/younesstime/index.html"
+  },
+  {
+    "revision": "96ffa3603b7f5c2897b0",
+    "url": "/younesstime/static/css/main.12e72a3d.chunk.css"
+  },
+  {
+    "revision": "182bbaed588e3eb596c5",
+    "url": "/younesstime/static/js/2.7b1c3fc3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/younesstime/static/js/2.7b1c3fc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96ffa3603b7f5c2897b0",
+    "url": "/younesstime/static/js/main.3e771276.chunk.js"
+  },
+  {
+    "revision": "22082d71e4dd57ebbf45",
+    "url": "/younesstime/static/js/runtime-main.38962596.js"
+  }
+]);
